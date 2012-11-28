8bedd0378acfa56beaee41b87053c047523d480c
Fernando Bass <ferbass@gmail.com>
2012-11-28 06:51:20 -0200

    uitabletable view

--- tableView1/tableView1/ViewController.m
+++ tableView1/tableView1/ViewController.m
@@ -9,13 +9,17 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    NSArray *carros;
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad
 {
+    carros = @[@"Gol", @"Fusca", @"Opala", @"Civic", @"Corolla", @"Ferrari"];
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
@@ -26,4 +30,23 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return carros.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    
+    cell.textLabel.text = [carros objectAtIndex:indexPath.row];
+    
+    return cell;
+}
+
 @end
+
+
+
+
+
