74c8183b0e12a96f9f364c08505ef4e8a9b07769
Fernando Bass <ferbass@gmail.com>
2012-11-27 23:28:55 -0200

    iniciando cast de tableview - 1

---
+++ tableView1/tableView1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tableView1
+//
+//  Created by Fernando Ribeiro on 11/27/12.
+//  Copyright (c) 2012 Fernando Bass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tableView1/tableView1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  tableView1
+//
+//  Created by Fernando Ribeiro on 11/27/12.
+//  Copyright (c) 2012 Fernando Bass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
